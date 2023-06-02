d4119d12c2e3aa4fc6ea33afd0ba8da12f794340
Dennis Boachie Boateng <dennisboachie9@gmail.com>
2023-06-02 16:43:44 +0000

    Implementing a stack

--- push.sh
+++ push.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
-
-# Read the repository URL from user input
-read -p "Enter the GitHub repository URL: " repository_url
-
-# Initialize the local repository as a Git repository
-git init
-
-# Add the remote repository URL
-git remote add origin "$repository_url"
-
-# Commit the changes
+git remote add origin https://github.com/Dennisblay/stack-go.git
 git add .
-git commit -m "Initial commit"
-
-# Push the changes to GitHub
-git push -u origin master
-
+git commit -m "Implementing a stack"
+git branch -M main
+git push -u origin main
